19ee035a2699bb8570440039790bb951cb75ec61
open.baker <dominik@schienstock.de>
2019-10-16 17:44:51 +1100

    Create Dockerfile from jupyter project and merge to CUDA container

---
+++ build_dockerfile.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# get Dockerfiles from Jupyter project
+# git clone https://github.com/jupyter/docker-stacks.git
+cd docker-stacks
+git pull
+cd ..
+
+# define base image
+cat Dockerfile.base > Dockerfile.tmp
+
+# get Dockerfile definitions and merge together
+jpt_dockerfiles=(
+	"datascience"
+	"scipy"
+	"minimal"
+	"base"	
+)
+
+for i in "${jpt_dockerfiles[@]}"
+do
+   cat docker-stacks/$i-notebook/Dockerfile | grep -v "FROM.*" >> Dockerfile.tmp
+done
+
+# define cecelialab part
+cat Dockerfile.cecelialab >> Dockerfile.tmp
+
+# create Dockerfile
+mv Dockerfile.tmp Dockerfile
