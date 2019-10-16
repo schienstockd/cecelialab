d30dcba53461b7450fbab3812f52a5b43829bbdf
open.baker <dominik@schienstock.de>
2019-10-16 12:16:18 +1100

    Add R script to install libraries

---
+++ install_libraries.R
@@ -0,0 +1,5 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager", repos="http://cran.rstudio.com/")
+
+BiocManager::install("openCyto")
+BiocManager::install("ggcyto")
